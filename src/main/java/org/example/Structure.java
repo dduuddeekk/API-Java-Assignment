73f8bc3eeaa05b3a05f500816042190d97f85099
dduuddeekk <jajajablablabla123@gmail.com>
2023-05-30 12:13:09 +0800

    Update Structure.java

--- src/main/java/org/example/Structure.java
+++ src/main/java/org/example/Structure.java
@@ -3,20 +3,20 @@ package org.example;
 public class Structure {
     public static String users(int id, String first_name, String last_name, String email, String phone_number, String type){
         String result = "\t\"id\" : "+id+",\n"+
-                "\t\"first_name\" : "+first_name+",\n"+
-                "\t\"last_name\" : "+last_name+",\n"+
-                "\t\"email\" : "+email+",\n"+
-                "\t\"phone_number\" : "+phone_number+",\n"+
-                "\t\"type\" : "+type+"\n";
+                "\t\"first_name\" : \""+first_name+"\",\n"+
+                "\t\"last_name\" : \""+last_name+"\",\n"+
+                "\t\"email\" : \""+email+"\",\n"+
+                "\t\"phone_number\" : \""+phone_number+"\",\n"+
+                "\t\"type\" : \""+type+"\"";
         return result;
     }
     public static String products(int id, int sellerId, String title, String description, int price, int stock){
         String result = "\t\"id\" : "+id+",\n"+
                 "\t\"seller\" : "+sellerId+",\n"+
-                "\t\"title\" : "+title+",\n"+
-                "\t\"description\" : "+description+",\n"+
+                "\t\"title\" : \""+title+"\",\n"+
+                "\t\"description\" : \""+description+"\",\n"+
                 "\t\"price\" : "+price+",\n"+
-                "\t\"stock\" : "+stock+",\n";
+                "\t\"stock\" : "+stock;
         return result;
     }
     public static String addresses(int userId, String type, String lineOne, String lineTwo, String city, String province, String postcode){
@@ -32,23 +32,23 @@ public class Structure {
     public static String orders(int id, int buyerId, String note, int total, int discount, String is_paid){
         String result = "\t\"id\" : "+id+",\n"+
                 "\t\"buyer\" : "+buyerId+",\n"+
-                "\t\"note\" : "+note+",\n"+
+                "\t\"note\" : \""+note+"\",\n"+
                 "\t\"total\" : "+total+",\n"+
                 "\t\"discount\" : "+discount+",\n"+
-                "\t\"is_paid\" : "+is_paid+"\n";
+                "\t\"is_paid\" : \""+is_paid+"\"";
         return result;
     }
     public static String order_details(int id, int productId, int quantity, int price){
         String result = "\t\"id\" : "+id+",\n"+
                 "\t\"product\" : "+productId+",\n"+
                 "\t\"quantity\" : "+quantity+",\n"+
-                "\t\"price\" : "+price+"\n";
+                "\t\"price\" : "+price;
         return result;
     }
     public static String reviews(int id, int star, String description){
         String result = "\t\"id\" : "+id+",\n"+
                 "\t\"star\" : "+star+",\n"+
-                "\t\"description\" : "+description+"\n";
+                "\t\"description\" : \""+description+"\"";
         return result;
     }
 }
